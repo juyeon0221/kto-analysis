23157f7838004dcc885e91f50778dd31193a11c7
juyeon <l.juyeon0221@gmail.com>
2019-04-27 17:15:43 +0900

    format document

--- kto_analysis.py
+++ kto_analysis.py
@@ -10,20 +10,21 @@ import pandas as pd
 
 driver = webdriver.Chrome()
 
-driver.get('https://twitter.com/search?l=&q=from%3AKor_Visitkorea%20since%3A2015-01-01&src=typd')
+driver.get(
+    'https://twitter.com/search?l=&q=from%3AKor_Visitkorea%20since%3A2019-01-01&src=typd')
 
 time.sleep(3)
 
 
-
-driver.find_element_by_name("session[username_or_email]").send_keys('01031406980')
+driver.find_element_by_name(
+    "session[username_or_email]").send_keys('01031406980')
 
 driver.find_element_by_name("session[password]").send_keys('Jenny0221@')
 
-driver.find_element_by_xpath("//*[@class='EdgeButton EdgeButton--primary EdgeButton--medium submit js-submit']").click()
+driver.find_element_by_xpath(
+    "//*[@class='EdgeButton EdgeButton--primary EdgeButton--medium submit js-submit']").click()
 
 last_height = driver.execute_script("return document.body.scrollHeight")
-
 
 
 while True:
@@ -41,39 +42,38 @@ while True:
     last_height = new_height
 
 
-
 list = list()
 
 con = driver.page_source
 
-html = BeautifulSoup(con,'html.parser')
+html = BeautifulSoup(con, 'html.parser')
 
-div = html.find_all("li",{"class":"js-stream-item stream-item stream-item"})
+div = html.find_all("li", {"class": "js-stream-item stream-item stream-item"})
 
 for d in div:
 
-    a1 = d.find("a",{"class":"tweet-timestamp js-permalink js-nav js-tooltip"})
+    a1 = d.find(
+        "a", {"class": "tweet-timestamp js-permalink js-nav js-tooltip"})
 
     title = a1["title"]
 
-    a2 = d.find_all("a",{"class":"twitter-hashtag pretty-link js-nav"})
+    a2 = d.find_all("a", {"class": "twitter-hashtag pretty-link js-nav"})
 
     for ah in a2:
 
         hashtag = ah.find("b").text
 
-        row = [title,hashtag]
+        row = [title, hashtag]
 
-        list.append(row) 
+        list.append(row)
 
-print ('list 수집 완료')
+print('list 수집 완료')
 
 data = pd.DataFrame(list)
 
-data.columns=['date', 'hashTag']
+data.columns = ['date', 'hashTag']
 
-data.to_csv('구석구석_전체.csv', encoding='UTF-8') 
-
+data.to_csv('구석구석_전체.csv', encoding='UTF-8')
 
 
 driver.close()
